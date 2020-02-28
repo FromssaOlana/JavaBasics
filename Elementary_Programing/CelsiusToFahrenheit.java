0fe6c77cf910f579544220f5a70e6cb59c1819d2
FromssaOlana <zz4396bs@metrostate.edu>
2020-02-28 11:56:43 -0600

    Celsius to Fahrenheit converter class added

---
+++ Elementary_Programing/CelsiusToFahrenheit.java
@@ -0,0 +1,23 @@
+package JavaBasics.Elementary_Programing;
+
+/*
+Write a program that reads a Celsius degree in
+a double value from the console, then converts it to Fahrenheit and displays the
+result. The formula for the conversion is as follows:
+fahrenheit = (9 / 5) * celsius + 32
+ */
+
+import java.util.Scanner;
+
+public class CelsiusToFahrenheit {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double celsius;
+        double fahrenheit;
+        System.out.println("Please enter Temp in Celsius: ");
+        celsius = input.nextDouble();
+        fahrenheit = (9/5)* celsius + 32;
+
+        System.out.println("Temp in fahrenheit is: "+ fahrenheit);
+    }
+}
